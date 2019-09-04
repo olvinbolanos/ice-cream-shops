d861838b3bd27fadcb740b2a89ec347dde3759f8
Olvin <olvin.bolanos@yahoo.com>
2019-09-04 10:14:14 -0700

    making a delete

--- src/component/Reviews/index.jsx
+++ src/component/Reviews/index.jsx
@@ -29,7 +29,36 @@ class Reviews extends Component {
     getReviewsFromApi = (reviewId) => {
         console.log(reviewId, '<----this is in reviewsFromApi')
 
-        axios.get(`${'https://cors-anywhere.herokuapp.com'}https://api.yelp.com/v3/businesses/`)
+        axios.get(`${'https://cors-anywhere.herokuapp.com'}https://api.yelp.com/v3/businesses/${reviewId}/reviews`, {
+            // required authorization format from API
+            headers: {
+                Authorization: `Bearer ${process.env.REACT_APP_YELP_KEY}`,
+            }
+        })
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+    renderEmptyState() {
+        return (
+            <h2 className='faultyInfo'>
+               `Hang tight! We are working on getting you the list of reviews about the store you requested! 
+            </h2>
+        )
+    }
+
+    renderReviews() {
+        const ReviewsList = this.state.reviews.map((review, i) => {
+            // return (
+            //     <div className="reviewsInfo" key={i}>
+            //         <h2>className</h2>
+            //     </div>
+            // )
+        })
     }
 
 
